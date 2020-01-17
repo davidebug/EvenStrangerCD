2279388bfb589c6e5b33271f715cb0f578b7003b
davidebug <33726212+davidebug@users.noreply.github.com>
2020-01-17 23:25:37 +0100

    Folders update

--- GameDesign project/Documentation/LevelDesignDocument/LDD_Plants&Paths/Plants/tileset_plants/Tileset for Tiled/bianco.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="bianco" tilewidth="32" tileheight="32" tilecount="1425" columns="57">
- <image source="../../tileset colori/bianco.png" width="1837" height="823"/>
-</tileset>
--- GameDesign project/Documentation/LevelDesignDocument/LDD_Plants&Paths/Plants/tileset_plants/Tileset for Tiled/viola.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="viola" tilewidth="32" tileheight="32" tilecount="1425" columns="57">
- <image source="../../tileset colori/dab.png" width="1837" height="823"/>
-</tileset>
